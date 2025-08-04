f4d26cbc17ba02d912fb54c8f12ff6340dfb8864
amoorali <amirfallahi222@gmail.com>
2025-08-04 21:52:06 +0330

    first tests of users api.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext context) : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")] // /api/users/{id}
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            if (user == null) return NotFound($"User with ID {id} not found.");
+            return Ok(user);
+        }
+    }
+}
